f8d3dd4db8aff91de0a0b8bd79be8bae880325e6
Krish Patel <krishrpatel@gmail.com>
2023-08-09 11:36:37 -0400

    4th button

--- WebsiteCode.js
+++ WebsiteCode.js
@@ -2,10 +2,10 @@ document.getElementById("linkvalue").style.display = "none";
 // Makes the Github Link button invisible on execution 
 
 class CodeProject {
-    constructor(Language, ProjectName, GitHubLink){
+    constructor(Language, ProjectName){
         this.Language = Language;
         this.ProjectName = ProjectName;
-        this.Git = GitHubLink;
+        //this.Git = GitHubLink;
     }
     
 };
@@ -22,9 +22,10 @@ function ChangeLabel(prog){
 document.getElementById("ProjDetails").style.fontSize = "xx-large";
 // Sets subheading text to a smaller font 
 
-let WebScrape = new CodeProject("Java", "URLReader.java", "https://github.com/KrishMay05/https://github.com/KrishMay05/Purdue23-24/blob/5e069b5b0bdc53a0cea2c2fedd87672397afc0e4/URLReader.java-24/blob/main/URLReader.java");
-let WebApp = new CodeProject("JavaScript", "WebsiteCode.js", "https://github.com/KrishMay05/Purdue23-https://github.com/KrishMay05/Purdue23-24/blob/88c0ab3bb25c947d8d963715fcc3f0d8a146d115/WebsiteCode.js/blob/main/WebsiteCode.js");
-let WebAppHTML = new CodeProject("HTML", "WebsiteRunner.html", "https://github.com/KrishMay05/Purdue23-24/blob/main/WebsiteRunner.html" );
+let WebScrape = new CodeProject("Java", "URLReader.java");
+let WebApp = new CodeProject("JavaScript", "WebsiteCode.js");
+let WebAppHTML = new CodeProject("HTML", "WebsiteRunner.html" );
+let FTC = new CodeProject("Java", "FTC Freight Frenzy")
 // creates the CodeProject objects with the relevant parameters 
 
 document.getElementById("Proj1").onclick = function() {
@@ -41,6 +42,11 @@ document.getElementById("Proj3").onclick = function(){
     ChangeLabel(WebAppHTML);
     document.getElementById("linkvalue").style.display = "block";
     document.getElementById("linkvalue").href="https://github.com/KrishMay05/Purdue23-24/blob/main/WebsiteRunner.html"; 
+}
+document.getElementById("Proj4").onclick = function(){
+    ChangeLabel(FTC);
+    document.getElementById("linkvalue").style.display = "block";
+    document.getElementById("linkvalue").href="https://github.com/KrishMay05/Freight_Frenzy_2021-22"; 
 }
 // Makes it so that on button click it shows subheading information on click and makes the Github link button visible again
 
